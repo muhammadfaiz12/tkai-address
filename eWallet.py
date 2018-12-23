7e03adc14d10cde997edf270e55dc8e85683d079
muhammad.faiz <faiz00.muhammad@gmail.com>
2018-12-23 19:22:28 +0700

    ketinggalan

--- eWallet.py
+++ eWallet.py
@@ -35,18 +35,16 @@ threshold_mid = math.floor(len(quorum_list)/2) + 1
 
 # DB model for account
 class Account(db.Model):
-    user_id    = db.Column(db.String, primary_key=True)
-    nama       = db.Column(db.String)
-    amount     = db.Column(db.Integer)
-    is_host    = db.Column(db.Boolean)
-
+    address_id    = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
+    user_id       = db.Column(db.INTEGER)
+    address    = db.Column(db.String)
     def __repr__(self):
-        return '<Account {0} {1} {2}>'.format(self.user_id, self.nama, self.amount)
+        return '<Account {0} {1} {2}>'.format(self.user_id, self.user_id, self.address)
 
 def init_account():
     account = Account.query.filter_by(user_id=host).first()
     if not account:
-        newAccount = Account(user_id=host, nama=hostname, amount=1000000000, is_host=True)
+        newAccount = Account(user_id=9999, address="Dummy Address")
         db.session.add(newAccount)
         db.session.commit()
 
@@ -84,130 +82,55 @@ def checkQuorum(threshold, ret_name):
         return wrapperCheckQuorum
     return decoratorCheckQuorum
 
-@bp.route('/ping',  methods=["POST"])
+@app.route('/ping',  methods=["POST"])
 def ping():
     pong = BERHASIL
     return jsonify({"pingReturn":pong})
 
-@bp.route('/getSaldo',  methods=["POST"])
-@checkQuorum(threshold_mid, "saldo")
-def getSaldo():
-    data = request.json
-    status_saldo = BERHASIL
-    try:
-        account = Account.query.filter_by(user_id=data["user_id"]).first()
-        if not account:
-            status_saldo = USER_ID_TIDAK_ADA
-            return jsonify({"saldo": status_saldo})
-        saldo = account.amount
-        return jsonify({"saldo": saldo})
-    except exc.SQLAlchemyError:
-        status_saldo = GAGAL_DATABASE
-        return jsonify({"saldo": status_saldo})
-    except Exception:
-        status_saldo = TIDAK_TERDEFINISI
-        return jsonify({"saldo": status_saldo})
-
-@bp.route('/getTotalSaldo',  methods=["POST"])
-@checkQuorum(threshold_high, "saldo")
-def getTotalSaldo():
-    data = request.json
-    status_total = BERHASIL
-    
-    try:
-        account = Account.query.filter_by(user_id=data["user_id"]).first()
-        if not account:
-            status_total = USER_ID_TIDAK_ADA
-            return jsonify({"saldo": status_total})
-        if not account.is_host:
-            url = "http://{}/ewallet/getTotalSaldo".format(quorum[account.user_id])
-            r = requests.post(url, json={"user_id": account.user_id})
-            return jsonify(r.json())
-        
-        saldo = 0
-        for q in quorum_list:
-            url = "http://{}/ewallet/getSaldo".format(quorum[q])
-            r = requests.post(url, json={"user_id":account.user_id})
-            print(r.json()["saldo"])
-            if r.json()["saldo"] >= 0:
-                saldo += r.json()["saldo"]
-            elif r.json()["saldo"] == -1:
-                regis_url = "http://{}/ewallet/register".format(quorum[q])
-                r_regis = requests.post(regis_url, json={"user_id":account.user_id, "nama": account.nama})
-                continue
-            elif r.json()["saldo"] < -1:
-                status_saldo = HOST_GAGAL
-                return jsonify({"saldo": status_saldo})
-
-        return jsonify({"saldo": saldo})
-
-    except requests.ConnectionError:
-        status_saldo = HOST_GAGAL
-        return jsonify({"registerReturn":status_saldo})
-
-    except Exception:
-        status_saldo = TIDAK_TERDEFINISI
-        return jsonify({"registerReturn":status_saldo})
-
-@bp.route('/register',  methods=["POST"])
-@checkQuorum(threshold_mid, "registerReturn")
-def register():
-    data = request.json
-    status_register = BERHASIL
-    try:
-        if data["user_id"] not in quorum:
-            raise ValueError("Tidak boleh mendaftar kalau bukan di quorum")
-        newAccount = Account(user_id=data["user_id"], nama=data["nama"], amount=0, is_host=False)
-        db.session.add(newAccount)
-        db.session.commit()
-        return jsonify({"registerReturn":status_register})
-    except exc.SQLAlchemyError as e:
-        status_register = GAGAL_DATABASE
-        return jsonify({"registerReturn":status_register})
-    except Exception:
-        status_register = TIDAK_TERDEFINISI
-        return jsonify({"registerReturn":status_register})
-
-@bp.route('/transfer',  methods=["POST"])
-@checkQuorum(threshold_mid, "transferReturn")
-def transfer():
-    data =  request.json
-    status_transfer = BERHASIL
-    try:
-        account = Account.query.filter_by(user_id=data["user_id"]).first()
-        if not account:
-            status_transfer = USER_ID_TIDAK_ADA
-            return jsonify({"transferReturn": status_transfer})
-
-        if data["nilai"] < 0 or data["nilai"] > 1000000000:
-            status_transfer = SYARAT_TRANSFER_GAGAL
-            return jsonify({"transferReturn": status_transfer})
-
-        account.amount += data["nilai"]
-        db.session.commit()
-
-        return jsonify({"transferReturn": status_transfer})
-    except exc.SQLAlchemyError:
-        status_transfer = GAGAL_DATABASE
-        return jsonify({"transferReturn": status_transfer})
-    except Exception:
-        status_transfer = TIDAK_TERDEFINISI
-        return jsonify({"transferReturn": status_transfer})
-
-
-@app.route('/substractSaldo', methods=["POST"])
-def substractSaldo():
+@app.route('/edit-address', methods=["POST"])
+def editAddress():
     try:
         data = request.json
         account = Account.query.filter_by(user_id=data["user_id"]).first()
         if not account:
-            return jsonify({"substractReturn": -1})
-        account.amount -= data["nilai"]
+            return jsonify({"status": -2, "message":"User Not Found"})
+        account.address = data["address"]
         db.session.commit()
 
-        return jsonify({"substractReturn": 1})
+        return jsonify({"edit-address": 1, "new-address": data["address"]})
     except Exception:
-        return jsonify({"substractReturn": -99})
+        return jsonify({"edit-address": -1})
+
+@app.route('/address', methods=["POST"])
+def addAddress():
+    try:
+        data = request.json
+        account = Account(user_id=data["user_id"], address=data["address"])
+        db.session.add(account)
+        db.session.commit()
+        if not account:
+            return jsonify({"add-address": -2, "message":"User record creation Failed"})
+        account.address = data["address"]
+        db.session.commit()
+
+        return jsonify({"add-address": 1, "id":account.user_id, "address": data["address"]})
+    except Exception as e:
+        print(e)
+        return jsonify({"add-address": -10})
+
+@app.route('/address', methods=["DELETE"])
+def deleteAddress():
+    try:
+        data = request.json
+        account = Account.query.filter_by(user_id=data["user_id"])
+        if not account:
+            return jsonify({"status": -2, "message":"User Not Found"})
+        db.session.delete(account)
+        db.session.commit()
+
+        return jsonify({"delete-address": 1})
+    except Exception as e:
+        return jsonify({"delete-address": -1, "message":str(e)})
 
 if __name__ == "__main__":
     app.register_blueprint(bp)
